8f630a18a28984a8b944a9f46811719360851edd
25704Kawashima <kawashima-t@hokugin-sw.co.jp>
2025-11-27 09:31:52 +0900

    second commit.

--- main.py
+++ main.py
@@ -1,148 +1,40 @@
-"""
-このファイルは、Webアプリのメイン処理が記述されたファイルです。
-"""
-
-############################################################
-# ライブラリの読み込み
-############################################################
 from dotenv import load_dotenv
-import logging
+import os
 import streamlit as st
-import utils
-from initialize import initialize
-import components as cn
-import constants as ct
-
-
-############################################################
-# 設定関連
-############################################################
-st.set_page_config(
-    page_title=ct.APP_NAME
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
 )
 
 load_dotenv()
+llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.5,streaming=True)
 
-logger = logging.getLogger(ct.LOGGER_NAME)
+st.title("LLMチャットアプリ")
 
+selected_chat = st.radio(
+    "チャットを選択してください", 
+    ("システムエンジニア", "歴史の専門家")
+)
 
-############################################################
-# 初期化処理
-############################################################
-try:
-    initialize()
-except Exception as e:
-    logger.error(f"{ct.INITIALIZE_ERROR_MESSAGE}\n{e}")
-    st.error(utils.build_error_message(ct.INITIALIZE_ERROR_MESSAGE), icon=ct.ERROR_ICON)
-    st.stop()
-
-# アプリ起動時のログ出力
-if not "initialized" in st.session_state:
-    st.session_state.initialized = True
-    logger.info(ct.APP_BOOT_MESSAGE)
+st.divider()
 
 
-############################################################
-# 初期表示
-############################################################
-# タイトル表示
-cn.display_app_title()
+input_message = st.text_input(label=f"{selected_chat}への質問を入力してください:")
 
-# サイドバー表示
-cn.display_sidebar()
+system_template = "あなたは{expert}です。質問に対してわかりやすく丁寧に答えてください。また、専門分野外の質問には答えず、その旨を伝えてください。"
+human_template = "{input}"
 
-# AIメッセージの初期表示
-cn.display_initial_ai_message()
+prompt = ChatPromptTemplate.from_messages([
+    SystemMessagePromptTemplate.from_template(system_template),
+    HumanMessagePromptTemplate.from_template(human_template),
+])
 
+if st.button("送信"):
+    st.divider()
 
-############################################################
-# スタイリング処理
-############################################################
-# 画面装飾を行う「CSS」を記述
-st.markdown(ct.STYLE, unsafe_allow_html=True)
-
-
-############################################################
-# チャット入力の受け付け
-############################################################
-chat_message = st.chat_input(ct.CHAT_INPUT_HELPER_TEXT)
-
-
-############################################################
-# 会話ログの表示
-############################################################
-try:
-    cn.display_conversation_log(chat_message)
-except Exception as e:
-    logger.error(f"{ct.CONVERSATION_LOG_ERROR_MESSAGE}\n{e}")
-    st.error(utils.build_error_message(ct.CONVERSATION_LOG_ERROR_MESSAGE), icon=ct.ERROR_ICON)
-    st.stop()
-
-
-############################################################
-# チャット送信時の処理
-############################################################
-if chat_message:
-    # ==========================================
-    # 会話履歴の上限を超えた場合、受け付けない
-    # ==========================================
-    # ユーザーメッセージのトークン数を取得
-    input_tokens = len(st.session_state.enc.encode(chat_message))
-    # トークン数が、受付上限を超えている場合にエラーメッセージを表示
-    if input_tokens > ct.MAX_ALLOWED_TOKENS:
-        with st.chat_message("assistant", avatar=ct.AI_ICON_FILE_PATH):
-            st.error(ct.INPUT_TEXT_LIMIT_ERROR_MESSAGE)
-            st.stop()
-    # トークン数が受付上限を超えていない場合、会話ログ全体のトークン数に加算
-    st.session_state.total_tokens += input_tokens
-
-    # ==========================================
-    # 1. ユーザーメッセージの表示
-    # ==========================================
-    logger.info({"message": chat_message})
-
-    res_box = st.empty()
-    with st.chat_message("user", avatar=ct.USER_ICON_FILE_PATH):
-        st.markdown(chat_message)
-    
-    # ==========================================
-    # 2. LLMからの回答取得
-    # ==========================================
-    res_box = st.empty()
-    try:
-        with st.spinner(ct.SPINNER_TEXT):
-            result = utils.execute_agent_or_chain(chat_message)
-    except Exception as e:
-        logger.error(f"{ct.GET_LLM_RESPONSE_ERROR_MESSAGE}\n{e}")
-        st.error(utils.build_error_message(ct.GET_LLM_RESPONSE_ERROR_MESSAGE), icon=ct.ERROR_ICON)
-        st.stop()
-    
-    # ==========================================
-    # 3. 古い会話履歴を削除
-    # ==========================================
-    utils.delete_old_conversation_log(result)
-
-    # ==========================================
-    # 4. LLMからの回答表示
-    # ==========================================
-    with st.chat_message("assistant", avatar=ct.AI_ICON_FILE_PATH):
-        try:
-            cn.display_llm_response(result)
-
-            logger.info({"message": result})
-        except Exception as e:
-            logger.error(f"{ct.DISP_ANSWER_ERROR_MESSAGE}\n{e}")
-            st.error(utils.build_error_message(ct.DISP_ANSWER_ERROR_MESSAGE), icon=ct.ERROR_ICON)
-            st.stop()
-    
-    # ==========================================
-    # 5. 会話ログへの追加
-    # ==========================================
-    st.session_state.messages.append({"role": "user", "content": chat_message})
-    st.session_state.messages.append({"role": "assistant", "content": result})
-
-
-############################################################
-# 6. ユーザーフィードバックのボタン表示
-############################################################
-cn.display_feedback_button()
+    messages = prompt.format_messages(expert=selected_chat,input=input_message)
+    result = llm(messages)
+    st.write("### 回答:")
+    st.write(result.content)
